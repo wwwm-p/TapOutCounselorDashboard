ed21a6ac01b834f7fa74eb0e55f7d875dfcec099
wwwm-p <miap2k10@gmail.com>
2026-03-27 14:29:28 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,43 @@
-const CACHE_NAME = "tapout-counselor-v1";
+const CACHE_NAME = "tapout-counselor-v2";
+
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/counselor-manifest.json",
+  "/icons/icon-192-counselor.png",
+  "/icons/icon-512-counselor.png"
+];
 
 self.addEventListener("install", (event) => {
   console.log("Service Worker installing...");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activated");
-  return self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
