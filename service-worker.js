1e9a7961caf56d74d598822c8b1c5d15c2e1fbbc
wwwm-p <miap2k10@gmail.com>
2026-03-28 08:42:37 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,33 @@
-const CACHE = "tapout-v1";
+const CACHE_NAME = "tapout-counselor-v2";
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(cache=>{
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/counselor-manifest.json"
-      ]);
-    })
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/counselor-manifest.json",
+  "/icons/icon-192-counselor.png",
+  "/icons/icon-512-counselor.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=> res || fetch(e.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
