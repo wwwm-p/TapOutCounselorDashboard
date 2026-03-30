8c5b06d4700b29bcb2cc77d2f4bfc75fee3dec48
wwwm-p <miap2k10@gmail.com>
2026-03-30 09:22:01 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,30 @@
-const CACHE_NAME = "counselor-cache-v3";
+const CACHE_NAME = "counselor-cache-v5";
 const ASSETS_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./counselor-manifest.json",
-  "./icons/icon-192-counselor.png",
-  "./icons/icon-512-counselor.png"
+  "/",
+  "/index.html",
+  "/counselor-manifest.json",
+  "/icons/icon-192-counselor.png",
+  "/icons/icon-512-counselor.png"
 ];
 
-// Install SW & cache files
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      Promise.all(
-        ASSETS_TO_CACHE.map(url =>
-          cache.add(url).catch(err => {
-            console.warn("Cache failed for:", url, err);
-          })
-        )
-      )
-    )
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Activate SW & clean old caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => key !== CACHE_NAME && caches.delete(key))
-      )
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// Fetch handler - cache first, then network
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
